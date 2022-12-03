98f9c3e0aa270e958f0c39b9ef1a811143aaf7e3
Joseph BS <jjbs1805@gmail.com>
2022-12-03 10:27:48 -0500

    uploud project to git

---
+++ src/main/java/com/abm/dev/DevApplication.java
@@ -0,0 +1,14 @@
+package com.abm.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+	}
+
+}
